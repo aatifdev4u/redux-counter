7fff2a7dcf6850d63754a9f9d886cb636bcbd88f
Mohammad Aatif <aatif@ipost.com>
2019-02-19 17:27:52 +0530

    Implemented redux in Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import './Counter.css';
+import { connect } from 'react-redux';
 
 
-export default class Counter extends Component {
+class Counter extends Component {
 
     state = {
         count: 0
     }
 
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
+       this.props.dispatch({type: 'INCREMENT'})
     }
 
     decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        })
+        this.props.dispatch({type: 'DECREMENT'})
     }
 
     render() {
@@ -25,10 +22,19 @@ export default class Counter extends Component {
             <div className='counter'>
                 <h4>Counter</h4>
                 <button className='increment' onClick = {this.increment}>+</button>
-                <span className='count'>{this.state.count}</span>
+                <span className='count'>{this.props.count}</span>
                 <button className='decrement' onClick = {this.decrement} >-</button>
             </div>
         );
     }
 }
+
+
+ function mapStateToProps(state) {
+     return {
+         count: state.count
+     }
+ } 
+
+export default connect(mapStateToProps)(Counter);
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const intialState = {
+    count: 0
+}
+
+function reducer(state= intialState, action ) {
+    switch(action.type){
+        case 'INCREMENT':
+        return {
+            count: state.count + 1
+        };
+        case 'DECREMENT':
+        return {
+            count: state.count - 1
+        };
+        default:
+        return state;
+    }
+}
+
+const store = createStore(reducer);
 
 const App = () => (
-    <div>
+    <Provider store={store}>
         <Counter/>
-    </div>
+    </Provider>
     );
 
 
